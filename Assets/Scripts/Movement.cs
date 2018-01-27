2c0afb7555c9b0d75c1ed9a8156b35e3f03a2f56
twig <twigwhite@gmail.com>
2018-01-27 09:52:37 -0600

    Movement is done, testing scene is for testing (should make a scene folder)

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour 
+{
+	public float speed;
+	private Vector3 _movement;
+	private Transform _transform;
+
+	private void Awake()
+	{
+		_movement = new Vector3();
+		_transform = transform;
+	}
+
+	/// <summary>
+	/// Might want to move this to a fixed update method.
+	/// </summary>
+	private void Update()
+	{
+		var x = Input.GetAxis("Horizontal");
+		var y = Input.GetAxis("Vertical");
+		_movement.x = x;
+		_movement.z = y;
+		// Normalized because on keyboard you can move at a higher speed going
+		// diagonal.
+		_movement.Normalize();
+
+		_transform.position += _movement * speed * Time.deltaTime;
+	}
+}
