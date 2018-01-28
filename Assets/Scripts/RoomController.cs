34de3611e8ff49a37095a6e77a25db9e5aff2355
Ironthighs <bob.ironthighs@gmail.com>
2018-01-28 11:54:09 -0600

    Implemented RoomController which handles activating all objects with an ObjectActivator script on it as a child of the room gameobject.

---
+++ Assets/Scripts/RoomController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class RoomController : MonoBehaviour 
+{
+
+    [SerializeField]
+    protected GameObject _roomObject;
+    protected List<ObjectActivator> _roomObjectsToActivate = new List<ObjectActivator>();
+
+
+
+    protected void Start()
+    {
+        _roomObjectsToActivate = _roomObject.GetComponentsInChildren<ObjectActivator>().ToList();
+	}
+	
+    protected void Update() 
+    {
+		
+	}
+
+    public void StartRoom()
+    {
+        foreach(var roomObj in _roomObjectsToActivate)
+        {
+            roomObj.ActivateObject();
+        }
+    }
+
+    public void PauseRoom()
+    {
+        foreach(var roomObj in _roomObjectsToActivate)
+        {
+            roomObj.DeactivateObject();
+        }
+    }
+
+    private void OnGUI()
+    {
+        //if (GUI.Button(new Rect(10, 10, 150, 50), "Start Room"))
+        //{
+        //    StartRoom();
+        //}
+    }
+}
